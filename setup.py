4f39d0ee409ae7134711d66b683842017c7bbca3
Darijo Kutlesa <darijo.kutlesa@moj-posao.net>
2019-03-24 16:02:43 +0100

    Initial setup

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
+import os
 
-from setuptools import setup
+project_root = os.getcwd()
+os.chdir(os.path.join(project_root, "rss_client"))
+os.system("npm install")
+os.system("npm run build")
+os.system("npm run serve")
+
+os.chdir(os.path.join(project_root, "rss_server"))
+os.system("pip install -r requirements.txt")
+os.system("python manage.py migrate")
+os.system("python manage.py runserver")
