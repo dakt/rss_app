fb6821c0a988af60361aeefce654d013a619307a
Darijo Kutlesa <darijo.kutlesa@moj-posao.net>
2019-03-24 16:14:58 +0100

    Playing with setup script

--- setup.py
+++ setup.py
@@ -3,12 +3,22 @@
 import os
 
 project_root = os.getcwd()
-os.chdir(os.path.join(project_root, "rss_client"))
-os.system("npm install")
-os.system("npm run build")
-os.system("npm run serve")
+#os.chdir(os.path.join(project_root, 'rss_client'))
 
-os.chdir(os.path.join(project_root, "rss_server"))
-os.system("pip install -r requirements.txt")
-os.system("python manage.py migrate")
-os.system("python manage.py runserver")
+#print('Installing rss client...')
+#os.system('npm install')
+#os.system('npm run build')
+
+
+def install_server():
+    print('Installing rss server...')
+    os.chdir(os.path.join(project_root, 'rss_server'))
+    os.system('python3 -m venv .venv')
+    os.system('source .venv/bin/activate')
+
+    # os.system('pip install -r requirements.txt')
+    # print('Running initial migrations...')
+    # os.system('python manage.py migrate')
+
+
+install_server()
